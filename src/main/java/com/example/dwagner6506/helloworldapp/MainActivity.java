d1405648d963397a53747cccce9228b8e1ca7ec0
David Wagner <david.karl.wagner@live.com>
2017-09-07 10:38:16 -0400

    Hello World splash with img

---
+++ src/main/java/com/example/dwagner6506/helloworldapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.dwagner6506.helloworldapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+//DAVID WAGNER  9/7/2017
+//Hello World
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+  //  private void changeActivities(){
+    //    Intent myIntent = new Intent(MainActivity.this, Main2Activity.class);
+    //    startActivityForResult(myIntent, 0);
+   // }
+
+}
